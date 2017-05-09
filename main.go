d4edad648ecd28a13442dcab029ad21fd158d7b5
dimtruck <dimalg@yahoo.com>
2017-05-08 20:37:00 -0500

    add os-log dependency

--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
-
+  "os"
+  "github.com/op/go-logging"
 )
 
 func main() {
+  var log = logging.MustGetLogger("example")
+  var format = logging.MustStringFormatter(
+  	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+  )
 	fmt.Println("Hello, cloud Again!")
+
+	// For demo purposes, create two backend for os.Stderr.
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+
+	// For messages written to backend2 we want to add some additional
+	// information to the output, including the used log level and the name of
+	// the function.
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+	// Only errors and more severe messages should be sent to backend1
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+
+	// Set the backends to be used.
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+
+	log.Info("info")
+	log.Notice("notice")
+	log.Warning("warning")
+	log.Error("err")
+	log.Critical("crit")
 }
